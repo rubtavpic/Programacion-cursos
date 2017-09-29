e89ecd228ee24c3200e364bdab36d55616a86cc2
rtavero <rtavero@us.es>
2017-09-29 12:32:46 +0200

    ordenadas las fotos por última fecha de creacion

--- curso_Python_Django_REST/Frikr/frikr/settings.py
+++ curso_Python_Django_REST/Frikr/frikr/settings.py
@@ -120,3 +120,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Logs all SQL queries in debug
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
--- curso_Python_Django_REST/Frikr/photos/views.py
+++ curso_Python_Django_REST/Frikr/photos/views.py
@@ -8,9 +8,9 @@ from photos.models import Photo
 
 def home(request):
     """Esta función devuelve el home de mi página"""
-    photos = Photo.objects.all()
+    photos = Photo.objects.all().order_by('-created_at')
     context = {
-        'photos_list' : photos
+        'photos_list' : photos[:5]
     }
 
     return render(request,'photos/home.html', context)
