9a66d14da99d6d551c49fda7fa999dc8e1e312f8
rtavero <rtavero@us.es>
2017-10-02 13:00:43 +0200

    Realizada la sesión 11 con las funciones de login y logout de la aplicación de user

--- curso_Python_Django_REST/Frikr/frikr/urls.py
+++ curso_Python_Django_REST/Frikr/frikr/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'photos.views.home', name='photos_home'),
     url(r'^photos/(?P<pk>[0-9]+)$','photos.views.detail', name='photo_detail'),
+
+    # Users URLS
+    url(r'^login$','users.views.login', name='users_login'),
+    url(r'^logout$','users.views.logout', name='users_logout')
 ]
