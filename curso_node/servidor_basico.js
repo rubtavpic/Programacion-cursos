3bc2c74f3ff18627fde7b61adb6706d5afec59ce
Ruben Tavero Picon <rubtavpic@Mac-mini-de-Ruben.local>
2017-11-04 18:44:44 +0100

    Creado el primer servidor de prueba

--- curso_node/servidor_basico.js
+++ curso_node/servidor_basico.js
@@ -2,8 +2,8 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Wake up, Neo...\n');
+    response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    response.end('<h3>Wake up, Neo...</h3>\n');
 });
 
 // Arrancar el servidor
